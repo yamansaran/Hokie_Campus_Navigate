e3c15511f91131b1de5edf0507f0709e21084585
yamansaran <yamansaran@gmail.com>
2023-04-13 11:39:36 -0400

    Icon Added, comments added

---
+++ app/src/main/java/com/biaszebra/hokiecampusnavigate/DataEditor.java
@@ -0,0 +1,16 @@
+package com.biaszebra.hokiecampusnavigate;
+
+import java.io.FileInputStream;
+
+public class DataEditor {
+
+    DataEditor(){
+
+    }
+    public void edit(FileInputStream is){//TODO
+
+
+
+    }
+
+}
--- app/src/main/java/com/biaszebra/hokiecampusnavigate/MainActivity.java
+++ app/src/main/java/com/biaszebra/hokiecampusnavigate/MainActivity.java
@@ -43,15 +43,15 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnClick(View view){
         ZoomageView customView = findViewById(R.id.myZoomageView);
         TextView pathView = findViewById(R.id.pathView);
-        TestWithSampleGraph runner = new TestWithSampleGraph();
+        TestWithSampleGraph runner = new TestWithSampleGraph();//create path generator
         EditText location = findViewById(R.id.location);
         String locationString = location.getText().toString();
         EditText destination = findViewById(R.id.destination);
         String destinationString = destination.getText().toString();
         InputStream is = this.getResources().openRawResource(R.raw.node_map);//NOTE MAP LOCATION
 
-        ArrayList<String[]> table = new ArrayList<>();
-        InputStream is2 = this.getResources().openRawResource(R.raw.sample_table);
+        ArrayList<String[]> table = new ArrayList<>();//table to hold point information
+        InputStream is2 = this.getResources().openRawResource(R.raw.sample_table);//point coordinate data file
         try (Scanner sc = new Scanner(is2, StandardCharsets.UTF_8.name())) {
             while (sc.hasNextLine()) {
                 String str = sc.nextLine();
@@ -60,47 +60,42 @@ public class MainActivity extends AppCompatActivity {
                 //System.out.println(res[0]+" "+res[1]+" "+res[2] + " added to table");
             }
         }
-        String testTest = runner.startRun(locationString,destinationString, is);
+        String testTest = runner.startRun(locationString,destinationString, is);//find path
         //System.out.println(testTest + "~~~~~~~~~~~~~");
-        testTest= testTest.replaceAll("\\s", "");
+        testTest= testTest.replaceAll("\\s", "");//filter output path
         testTest= testTest.replaceAll("\\[", "");
         testTest= testTest.replaceAll("\\]", "");
         //System.out.println(testTest + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        pathView.setText(testTest);
-        //TODO
-        /*
-            Create string array for coordinates from testTest
-            add csv file with coordinates
-            surround paint in for loop to draw paths
-         */
+        pathView.setText(testTest);//show path in pathView
+
 
         //customView.onDrawCo( 0,0,100,100, canvas);
         //Bitmap myBitmap = BitmapFactory.decodeFile("res/drawable/simple_node_map_shrunk.png");
 
-        Bitmap newBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_graph_cropped);
-        Bitmap myBitmap = Bitmap.createScaledBitmap(newBitmap, customView.getWidth(), customView.getHeight(), true);
+        Bitmap newBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_graph_cropped);//replacing image with copy image that will be painted on
+        Bitmap myBitmap = Bitmap.createScaledBitmap(newBitmap, customView.getWidth(), customView.getHeight(), true);//I do not know why this is here, but it wont work without  copy of a copy
         //Bitmap myBitmap = ((BitmapDrawable)customView.getDrawable()).getBitmap();
         //System.out.println(myBitmap.getHeight());
-        Bitmap tempBitmap = myBitmap.copy(myBitmap.getConfig(), true);
+        Bitmap tempBitmap = myBitmap.copy(myBitmap.getConfig(), true);//copy as previous bitmaps are immutable
         //Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_graph_cropped);
         Canvas tempCanvas = new Canvas(tempBitmap);
         tempCanvas.drawBitmap(myBitmap, 0, 0, null);
 
-        Paint p = new Paint();
+        Paint p = new Paint();//route paint, edit here
         p.setColor(Color.RED);
         p.setStrokeWidth(15);
 
-        Paint explorer = new Paint();
+        Paint explorer = new Paint();//TODO remove explorer line
         explorer.setColor(Color.BLACK);
         explorer.setStrokeWidth(3);
 
-        tempCanvas.drawLine(0,0,tempBitmap.getWidth(),tempBitmap.getHeight(),explorer);
+        //tempCanvas.drawLine(0,0,tempBitmap.getWidth(),tempBitmap.getHeight(),explorer);//Explorer line to span image
         //tempCanvas.drawLine(1000,1000,2000,1000,p);
         //tempCanvas.drawLine(2000,1000,1500,1600,p);
-        String[] res = testTest.split("[,]", 0);
+        String[] res = testTest.split("[,]", 0);//array of steps along path
         //System.out.println(res[0] + " " + res[3]);
-        //ArrayList<Integer> temp = returnCor(res[0]); //TODO do this
-        for(int i = 0; i < res.length;i++){
+        //ArrayList<Integer> temp = returnCor(res[0]);
+        for(int i = 0; i < res.length;i++){//loop to paint lines along map
             //System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+res[i]);
             if(i==(res.length-1)){
 
@@ -115,12 +110,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                int xCur = (int)(coorCurrent.get(0));
+                int xCur = (int)(coorCurrent.get(0));//paint start and end point
                 int yCur = (int)(coorCurrent.get(1));
                 int xNext = (int)(coorNext.get(0));
                 int yNext = (int)(coorNext.get(1));
 
-                xCur = scalingMod(coreXDim,xdim,xCur);
+                xCur = scalingMod(coreXDim,xdim,xCur);//scaled to make up for different screen resolutions
                 yCur = scalingMod(coreYDim,ydim,yCur);
                 xNext = scalingMod(coreXDim,xdim,xNext);
                 yNext = scalingMod(coreYDim,ydim,yNext);
@@ -135,12 +130,12 @@ public class MainActivity extends AppCompatActivity {
         customView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
     }
 
-    public Double doubleDivision(Double x, Double y) {
+    public Double doubleDivision(Double x, Double y) {//real division
         Double z = (double) x / y;
         return z;
 
     }
-    public int scalingMod(int coreDim, int newDim, int scalar){
+    public int scalingMod(int coreDim, int newDim, int scalar){//resolution scaling
       double factor = (double) newDim/coreDim;
       double result = (double) scalar*factor;
       int resultProcessed = (int)result;
